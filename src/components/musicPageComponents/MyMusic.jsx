ed3ca1f533ef6748206ec6682536d786192ad904
Szymon <szymon@MacBook-Pro-Mac-2.local>
2022-01-11 10:52:55 +0000

    Small improvements to audio player

--- src/components/musicPageComponents/MyMusic.jsx
+++ src/components/musicPageComponents/MyMusic.jsx
@@ -13,8 +13,9 @@ const MyMusic = () => {
     const [currentSong, setCurrentSong] = useState(musicAudioData.tracks[0])
     const [isMuted, setIsMuted] = useState(false)
     const [currentTime, setCurrentTime] = useState(0)
-
-
+    const [audio, setAudio] = useState()
+    const [audioDuration, setAudioDuration] = useState(0)
+    const [audioVolume, setAudioVolume] = useState(1)
 
     return (
         <div id='my-music'>
@@ -28,58 +29,86 @@ const MyMusic = () => {
                     </p>
                 </header>
                 <div>
-                    <audio id="audio" src={process.env.PUBLIC_URL + '/audio/' + currentSong.filepath} onTimeUpdate={(e)=>{
-                        setCurrentTime(e.target.currentTime)
-                        }}></audio>
+                    <audio 
+                        id="audio" 
+                        src={process.env.PUBLIC_URL + '/audio/' + currentSong.filepath} 
+                        onLoadedMetadata={(e) => {
+                                        setAudio(e.target)
+                                        setAudioDuration(e.target.duration)
+                                        }}
+                        onTimeUpdate={(e)=>{
+                                    e.preventDefault()
+                                    setCurrentTime(e.target.currentTime)
+                                    }}
+                    />
                 </div>
                 <div className='audio-player-full'>
                     <div className='audio-player-top-bar'>
-                        <div onClick={() => {
+                        <div 
+                            onClick={() => {
                             isPlaying ? setIsPlaying(false) : setIsPlaying(true);
-                            isPlaying ? document.getElementById('audio').pause() : document.getElementById('audio').play()
-                            }}>
+                            isPlaying ? audio.pause() : audio.play()
+                            }}
+                        >
                             {isPlaying ? <img src={pausebutton} width='90' alt='pause'/> : <img src={playbutton} width='90' alt='play'/>}
                         </div>
                         <div className='audio-player-currentsonginfo'>
-                            <div><b>{currentSong.artist}</b></div>
-                            <div>{currentSong.title}</div>  
+                            <div>
+                                <b>{currentSong.artist}</b>
+                            </div>
+                            <div>
+                                {currentSong.title}
+                            </div>  
                         </div>
                     </div>
                     <div className='audio-player-track-nav-bar'>
                         <div className='audio-player-track-nav-button-container'>
-                            <div className='audio-player-navbutton' onClick={()=>{
-                                setCurrentTime(0)
-                                currentSong.number === 1 ? setCurrentSong(musicAudioData.tracks[musicAudioData.tracks.length - 1]) : setCurrentSong(musicAudioData.tracks[currentSong.number - 2])
-                                setIsPlaying(true);
-                                setTimeout(()=>{document.getElementById('audio').play()}, 50)
-                                }}>
-                                    <img src={prevbutton} width='28' alt='prev'/>
-                                </div>
-                            <div className='audio-player-navbutton' onClick={()=>{
-                                setCurrentTime(0)
-                                currentSong.number === musicAudioData.tracks.length ? setCurrentSong(musicAudioData.tracks[0]) : setCurrentSong(musicAudioData.tracks[currentSong.number])
-                                setIsPlaying(true);
-                                setTimeout(()=>{document.getElementById('audio').play()}, 50)
-                                }}>
+                            <div className='audio-player-navbutton' 
+                                onClick={()=>{
+                                        setCurrentTime(0)
+                                        currentSong.number === 1 ? setCurrentSong(musicAudioData.tracks[musicAudioData.tracks.length - 1]) : setCurrentSong(musicAudioData.tracks[currentSong.number - 2])
+                                        setIsPlaying(true);
+                                        setTimeout(()=>{audio.play()}, 50)
+                                }}
+                            >
+                                <img src={prevbutton} width='28' alt='prev'/>
+                            </div>
+                            <div className='audio-player-navbutton' 
+                                onClick={()=>{
+                                        setCurrentTime(0)
+                                        currentSong.number === musicAudioData.tracks.length ? setCurrentSong(musicAudioData.tracks[0]) : setCurrentSong(musicAudioData.tracks[currentSong.number])
+                                        setIsPlaying(true);
+                                        setTimeout(()=>{audio.play()}, 50)
+                                }}
+                            >
                                     <img src={nextbutton} width='28' alt='next'/>
-                                </div>
+                            </div>
                         </div>
                         <div className='audio-player-progressbar-container'>
-                            <input type='range' step='0.01' className='audio-player-progressbar' value={(100 / document.getElementById("audio").duration) * currentTime} onChange={(e) => {
-                                e.preventDefault(); 
-                                const duration = document.getElementById("audio").duration
-                                document.getElementById("audio").currentTime = (e.target.value / 100) * duration; 
-                            }}>
-                            </input>
+                            <input 
+                                className='audio-player-progressbar'
+                                type='range' 
+                                step='0.01' 
+                                value={currentTime}
+                                max={audioDuration}
+                                onChange={(e) => {
+                                            e.preventDefault(); 
+                                            audio.currentTime = e.target.value
+                                        }}
+                            />
                         </div>
                         <div className='audio-player-time'>
                             {timeFormat(currentTime)} / {currentSong.duration}
                         </div>
-                        <div className='audio-player-volume' onClick={() => {
-                            isMuted ? setIsMuted(false) : setIsMuted(true)
-                            isMuted ? document.getElementById('audio').volume = 0 : document.getElementById('audio').volume = 1
-                            }}>
-                            {isMuted ? <img src={volumebutton} width='24' alt='volume'/> : <img src={mutebutton} width='24' alt='mute'/>}
+                        <div className='audio-player-volume'>
+                            <div  
+                                onClick={() => {
+                                        isMuted ? setIsMuted(false) : setIsMuted(true)
+                                        isMuted ? audio.volume = 1 : audio.volume = 0
+                                }}
+                            >
+                                {isMuted ? <img src={mutebutton} width='24' alt='mute'/> : <img src={volumebutton} width='24' alt='volume'/>}
+                            </div>
                         </div>
                     </div>
                     <div className='audio-player-track-list'>
@@ -88,20 +117,22 @@ const MyMusic = () => {
                                         setCurrentTime(0)
                                         setCurrentSong(musicAudioData.tracks[track.number - 1]);
                                         setIsPlaying(true);
-                                        setTimeout(()=>{document.getElementById('audio').play()}, 50)
+                                        setTimeout(()=>{audio.play()}, 50)
                                     }}>
                                         <div>
                                             <div>{track.number}. {track.artist} {track.title !== '' ? ' - ' : null} {track.title}</div>
                                         </div>
-                                        <div>{track.duration}</div>
+                                        <div>
+                                            {track.duration}
+                                        </div>
                                     </div> 
                         })}
                     </div>
-                    <div className='audio-player-audio-visualiser'>
+                    {/* <div className='audio-player-audio-visualiser'>
                             <div id="content">
                                 <canvas id="canvas"></canvas>
                             </div>
-                    </div>
+                    </div> */}
                 </div>
             </div>
         </div>
